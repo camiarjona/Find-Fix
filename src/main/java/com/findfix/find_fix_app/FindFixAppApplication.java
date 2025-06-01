4d28c0f27eae0df67e45859be3a9ae05e5b5db58
camiarjona <camarjona01@gmail.com>
2025-06-01 15:25:15 -0300

    Initial commit

---
+++ src/main/java/com/findfix/find_fix_app/FindFixAppApplication.java
@@ -0,0 +1,13 @@
+package com.findfix.find_fix_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindFixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindFixAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findfix/find_fix_app/FindFixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findfix.find_fix_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindFixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
