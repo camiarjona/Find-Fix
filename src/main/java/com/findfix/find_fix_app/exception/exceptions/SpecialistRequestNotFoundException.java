a8148aeca28003dc92ea65186b228579c00f42d4
camiarjona <camarjona01@gmail.com>
2025-06-05 21:28:04 -0300

    Manejo de errores

---
+++ src/main/java/com/findfix/find_fix_app/exception/exceptions/SpecialistRequestNotFoundException.java
@@ -0,0 +1,7 @@
+package com.findfix.find_fix_app.exception.exceptions;
+
+public class SpecialistRequestNotFoundException extends RuntimeException {
+    public SpecialistRequestNotFoundException(String message) {
+        super(message);
+    }
+}
