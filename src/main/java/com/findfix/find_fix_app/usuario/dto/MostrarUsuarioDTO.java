b2d054dfb92a8720ed017b3dc2bdb8a1632e91a1
camiarjona <camarjona01@gmail.com>
2025-06-10 18:16:50 -0300

    Creacion dtos usuario

---
+++ src/main/java/com/findfix/find_fix_app/usuario/dto/MostrarUsuarioDTO.java
@@ -0,0 +1,35 @@
+package com.findfix.find_fix_app.usuario.dto;
+
+import com.findfix.find_fix_app.rol.model.Rol;
+import com.findfix.find_fix_app.usuario.model.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//dto para ver lista de usuarios desde admin
+public class MostrarUsuarioDTO {
+    String nombre;
+    String apellido;
+    String email;
+    Set<String> rolesUsuario;
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\nApellido: " + apellido + "\nEmail: " + email
+                + "\nRoles: " + String.join(",", rolesUsuario);
+    }
+
+    public MostrarUsuarioDTO(Usuario usuario) {
+        this.nombre = usuario.getNombre();
+        this.apellido = usuario.getApellido();
+        this.email = usuario.getEmail();
+        this.rolesUsuario = usuario.getRoles().stream().map(Rol::getNombre).collect(Collectors.toSet());
+    }
+
+}
