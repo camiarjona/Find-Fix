724b612dfcb855753fcf935551a3a4db37e9d579
BelenIFigueroa <belenirmafigueroa@gmail.com>
2025-06-14 21:10:26 -0300

    Excepciones para Solicitud Especialista

---
+++ src/main/java/com/findfix/find_fix_app/exception/exceptions/SolicitudEspecialistaException.java
@@ -0,0 +1,7 @@
+package com.findfix.find_fix_app.exception.exceptions;
+
+public class SolicitudEspecialistaException extends Exception {
+    public SolicitudEspecialistaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/findfix/find_fix_app/exception/exceptions/SolicitudEspecialistaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.findfix.find_fix_app.exception.exceptions;
+
+public class SolicitudEspecialistaNotFoundException extends Exception {
+    public SolicitudEspecialistaNotFoundException(String message) {
+        super(message);
+    }
+}
