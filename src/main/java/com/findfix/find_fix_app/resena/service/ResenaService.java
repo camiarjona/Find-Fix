cb8d08b695971e82a1dee4d55912e94c7812d192
camiarjona <camarjona01@gmail.com>
2025-06-17 20:05:59 -0300

    Reseña

--- src/main/java/com/findfix/find_fix_app/resena/service/ResenaService.java
+++ src/main/java/com/findfix/find_fix_app/resena/service/ResenaService.java
@@ -1,15 +1,10 @@
 package com.findfix.find_fix_app.resena.service;
 
-import com.findfix.find_fix_app.especialista.model.Especialista;
-import com.findfix.find_fix_app.exception.exceptions.ResenaNotFoundException;
-import com.findfix.find_fix_app.exception.exceptions.SpecialistRequestNotFoundException;
-import com.findfix.find_fix_app.exception.exceptions.TrabajoAppNotFoundException;
-import com.findfix.find_fix_app.exception.exceptions.UserNotFoundException;
+import com.findfix.find_fix_app.utils.exception.exceptions.*;
 import com.findfix.find_fix_app.resena.dto.CrearResenaDTO;
-import com.findfix.find_fix_app.resena.dto.MostrarResenaDTO;
 import com.findfix.find_fix_app.resena.model.Resena;
-import com.findfix.find_fix_app.trabajo.trabajoExterno.dto.TrabajoExternoDTO;
-import com.findfix.find_fix_app.usuario.model.Usuario;
+import com.findfix.find_fix_app.utils.exception.exceptions.TrabajoAppNotFoundException;
+import com.findfix.find_fix_app.utils.exception.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +13,10 @@ import java.util.Optional;
 @Service
 public interface ResenaService {
     Resena crearResena(CrearResenaDTO DTO) throws TrabajoAppNotFoundException, UserNotFoundException;
-    Optional<Resena> buscarPorTrabajoId(Long id) throws ResenaNotFoundException;
+    Optional<Resena> buscarPorId(Long id) throws ResenaNotFoundException;
     Optional<Resena> buscarPorTrabajoTitulo(String titulo) throws ResenaNotFoundException, TrabajoAppNotFoundException;
-    List<CrearResenaDTO> ResenasDeMisTrabajos() throws UserNotFoundException, SpecialistRequestNotFoundException;
-    List<CrearResenaDTO> ResenasHechasPorMi() throws UserNotFoundException;
+    List<Resena> resenasDeMisTrabajos() throws UserNotFoundException, EspecialistaExcepcion, EspecialistaNotFoundException;
+    List<Resena> resenasHechasPorMi() throws UserNotFoundException;
     public void borrarResena(Long id) throws ResenaNotFoundException;
 
 }
