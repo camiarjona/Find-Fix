0887bd8cd7081351ce3049ae7dedc425df2cfc8b
BelenIFigueroa <belenirmafigueroa@gmail.com>
2025-06-17 11:55:20 -0300

    Cambios en DTOS de Especialista y SolicitudEspecialista

--- src/main/java/com/findfix/find_fix_app/especialista/dto/ActualizarOficioEspDTO.java
+++ src/main/java/com/findfix/find_fix_app/especialista/dto/ActualizarOficioEspDTO.java
@@ -1,21 +1,9 @@
 package com.findfix.find_fix_app.especialista.dto;
-
-import com.findfix.find_fix_app.especialista.model.Especialista;
-import com.findfix.find_fix_app.oficio.service.OficioService;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.Data;
-import org.apache.coyote.Response;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.HashSet;
 import java.util.Set;
 
 public record ActualizarOficioEspDTO (
-        Set<Long> agregar ,
-        Set<Long> eliminar){
+        Set<String> agregar ,
+        Set<String> eliminar){
 
     public ActualizarOficioEspDTO {
         if ((agregar == null || agregar.isEmpty()) &&
--- src/main/java/com/findfix/find_fix_app/solicitudEspecialista/dto/MostrarSolicitudEspecialistaAdminDTO.java
+++ src/main/java/com/findfix/find_fix_app/solicitudEspecialista/dto/MostrarSolicitudEspecialistaAdminDTO.java
@@ -2,11 +2,9 @@ package com.findfix.find_fix_app.solicitudEspecialista.dto;
 
 import java.time.LocalDate;
 
-public record MostrarSolicitudEspecialistaDTO(
+public record MostrarSolicitudEspecialistaAdminDTO(
         LocalDate fechaSolicitud,
         String estado,
-        String email,
-        String respuesta
-        ) {
-
+        String email
+) {
 }
