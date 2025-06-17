1561e8103f9e995bc21d780e0bf004666d9d2153
camiarjona <camarjona01@gmail.com>
2025-06-17 18:36:39 -0300

    api response

--- src/main/java/com/findfix/find_fix_app/trabajo/trabajoExterno/controller/TrabajoExternoController.java
+++ src/main/java/com/findfix/find_fix_app/trabajo/trabajoExterno/controller/TrabajoExternoController.java
@@ -6,6 +6,7 @@ import com.findfix.find_fix_app.trabajo.trabajoExterno.dto.ModificarTrabajoExter
 import com.findfix.find_fix_app.trabajo.trabajoExterno.dto.TrabajoExternoDTO;
 import com.findfix.find_fix_app.trabajo.trabajoExterno.model.TrabajoExterno;
 import com.findfix.find_fix_app.trabajo.trabajoExterno.service.TrabajoExternoService;
+import com.findfix.find_fix_app.utils.apiResponse.ApiResponse;
 import com.findfix.find_fix_app.utils.exception.exceptions.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/trabajos-externos")
@@ -27,59 +29,55 @@ public class TrabajoExternoController {
 
     @PostMapping("/agregar")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<Map<String, Object>> crearTrabajo(@Valid @RequestBody CrearTrabajoExternoDTO dto) throws UserNotFoundException, EspecialistaNotFoundException {
+    public ResponseEntity<ApiResponse<TrabajoExternoDTO>> crearTrabajo(@Valid @RequestBody CrearTrabajoExternoDTO dto) throws UserNotFoundException, EspecialistaNotFoundException {
         TrabajoExterno trabajo = trabajoExternoService.crearTrabajoExterno(dto);
-        Map<String, Object> response = new HashMap<>();
-        response.put("Mensaje: ", "Trabajo externo creado con éxito✅.");
-        response.put("Trabajo externo", new TrabajoExternoDTO(trabajo));
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "Trabajo externo creado con éxito✅",
+                new TrabajoExternoDTO(trabajo)));
     }
 
     @GetMapping("/mis-trabajos")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<?> obtenerTodos() throws UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
+    public ResponseEntity<ApiResponse<List<TrabajoExternoDTO>>> obtenerTodos() throws UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
         List<TrabajoExterno> misTrabajos = trabajoExternoService.obtenerMisTrabajos();
-        Map<String, Object> response = new HashMap<>();
-        response.put("Mensaje", "Lista de trabajos encontrada con éxito✅");
-        response.put("\uD83D\uDEE0️Mis trabajos\uD83D\uDEE0️", misTrabajos.stream().map(TrabajoExternoDTO::new));
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "\uD83D\uDD27Mis trabajos\uD83D\uDD27",
+                misTrabajos.stream().map(TrabajoExternoDTO::new).toList()
+        ));
     }
 
     @PatchMapping("/modificar/{titulo}")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<Map<String, String>> modificarTrabajo(@PathVariable String titulo, @Valid @RequestBody ModificarTrabajoExternoDTO dto) throws TrabajoExternoNotFoundException, OficioNotFoundException, UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
+    public ResponseEntity<ApiResponse<String>> modificarTrabajo(@PathVariable String titulo, @Valid @RequestBody ModificarTrabajoExternoDTO dto) throws TrabajoExternoNotFoundException, OficioNotFoundException, UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
         trabajoExternoService.modificarTrabajoExterno(titulo, dto);
-        Map<String, String> response = new HashMap<>();
-        response.put("Mensaje: ", "Trabajo externo actualizado con éxito☑️");
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new ApiResponse<>(
+                "Trabajo externo actualizado con éxito☑️",
+                "Consulte la lista de trabajos para corroborar los cambios."));
     }
 
     @DeleteMapping("/eliminar/{titulo}")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<Map<String, String>> eliminarTrabajo(@PathVariable String titulo) throws UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
+    public ResponseEntity<ApiResponse<String>> eliminarTrabajo(@PathVariable String titulo) throws UserNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
         trabajoExternoService.borrarTrabajoExternoPorTitulo(titulo);
-        Map<String, String> response = new HashMap<>();
-        response.put("Mensaje: ", "Trabajo externo eliminado con éxito✅");
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "Trabajo externo eliminado con éxito✅", "{}"));
     }
 
     @PatchMapping("/actualizar-estado/{titulo}/{estado}")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<?> actualizarEstado(@PathVariable String titulo, @PathVariable String estado) throws UserNotFoundException, TrabajoAppNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
+    public ResponseEntity<ApiResponse<String>> actualizarEstado(@PathVariable String titulo, @PathVariable String estado) throws UserNotFoundException, TrabajoAppNotFoundException, TrabajoExternoException, EspecialistaNotFoundException {
         trabajoExternoService.actualizarEstado(titulo, estado);
-        Map<String, Object> response = new HashMap<>();
-        response.put("Mensaje", "Estado actualizado con éxito☑️");
-        response.put("Estado", estado);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "Estado actualizado con éxito☑️",
+                "Estado: " + estado));
     }
 
     @GetMapping("/filtrar")
     @PreAuthorize("hasRole('ESPECIALISTA')")
-    public ResponseEntity<?> filtrarMisTrabajos(@RequestBody BuscarTrabajoExternoDTO filtro) throws UserNotFoundException, EspecialistaNotFoundException, TrabajoExternoException {
+    public ResponseEntity<ApiResponse<List<TrabajoExternoDTO>>> filtrarMisTrabajos(@RequestBody BuscarTrabajoExternoDTO filtro) throws UserNotFoundException, EspecialistaNotFoundException, TrabajoExternoException {
         List<TrabajoExterno> trabajos = trabajoExternoService.filtrarTrabajosExternos(filtro);
-        Map<String, Object> response = new HashMap<>();
-        response.put("Mensaje", "Lista de trabajos encontrada️☑️️");
-        response.put("Trabajos", trabajos.stream().map(TrabajoExternoDTO::new));
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "Coincidencias⬇️",
+                trabajos.stream().map(TrabajoExternoDTO::new).toList()));
     }
 }
