86a0825d6b3b994409ef8a57b9641f576676f80e
camiarjona <camarjona01@gmail.com>
2025-06-18 00:43:52 -0300

    Specifications y transactional

--- src/main/java/com/findfix/find_fix_app/trabajo/trabajoApp/repository/TrabajoAppRepository.java
+++ src/main/java/com/findfix/find_fix_app/trabajo/trabajoApp/repository/TrabajoAppRepository.java
@@ -4,13 +4,14 @@ import com.findfix.find_fix_app.especialista.model.Especialista;
 import com.findfix.find_fix_app.trabajo.trabajoApp.model.TrabajoApp;
 import com.findfix.find_fix_app.usuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TrabajoAppRepository extends JpaRepository<TrabajoApp,Long> {
+public interface TrabajoAppRepository extends JpaRepository<TrabajoApp,Long>, JpaSpecificationExecutor<TrabajoApp> {
     Optional<TrabajoApp> findByTitulo(String titulo);
     List<TrabajoApp> findByEspecialista(Especialista especialista);
     List<TrabajoApp> findByUsuario(Usuario usuario);
