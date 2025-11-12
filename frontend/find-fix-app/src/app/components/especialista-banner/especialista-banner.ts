b83e73f77dc06b87a5f01d93780ed509ccb14d52
camiarjona <camarjona01@gmail.com>
2025-11-12 17:06:45 -0300

    banner de especialista (home)

---
+++ frontend/find-fix-app/src/app/components/especialista-banner/especialista-banner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-especialista-banner',
+  imports: [],
+  templateUrl: './especialista-banner.html',
+  styleUrl: './especialista-banner.css',
+})
+export class EspecialistaBanner {
+
+}
