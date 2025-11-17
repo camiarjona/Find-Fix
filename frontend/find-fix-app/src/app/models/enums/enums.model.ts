bf628806cb68e3145b13404116be3a5bd62fe66c
BelenIFigueroa <belenirmafigueroa@gmail.com>
2025-11-17 20:19:36 -0300

    Creacion de layout y models cliente

---
+++ frontend/find-fix-app/src/app/models/enums/enums.model.ts
@@ -0,0 +1,5 @@
+/*Estados para la solicitud de especialista */
+
+export type EstadoSolicitud = 'Pendiente' | 'Aceptado' | 'Rechazado';
+
+export type EstadoTrabajo = 'Creado' | 'En proceso' | 'En revision' | 'Finalizado';
