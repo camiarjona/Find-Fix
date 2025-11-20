a6867b721db242b62e99ce24babbe9b9b3e633b0
camiarjona <camarjona01@gmail.com>
2025-11-20 19:37:51 -0300

    retoques admin dashboard

--- frontend/find-fix-app/src/app/app.routes.ts
+++ frontend/find-fix-app/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Home } from './pages/home/home.page';
 import { SeleccionarRolPage } from './pages/seleccionar-rol/seleccionar-rol.page';
 import { AdminDahboardComponent } from './components/admin-components/admin-dahboard-component/admin-dahboard-component';
 import { OficiosListAdminComponent } from './components/admin-components/oficios-list-admin-component/oficios-list-admin-component';
-import { Component } from '@angular/core';
 import { RolesListAdminComponent } from './components/admin-components/roles-list-admin-component/roles-list-admin-component';
 import { Routes } from '@angular/router';
 import { ClienteLayout } from './layouts/cliente/cliente-layout/cliente-layout';
@@ -36,6 +35,10 @@ export const routes: Routes = [
   {
     path: 'admin/oficios',
     component: OficiosListAdminComponent,
+  },
+  {
+    path: 'admin/roles',
+    component: RolesListAdminComponent,
   },
 
   // Rutas Privadas del Cliente
