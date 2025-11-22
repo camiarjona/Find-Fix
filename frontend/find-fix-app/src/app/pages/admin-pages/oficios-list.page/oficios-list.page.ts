aff3bab8a9346f653725f7307fc80263f023acfe
camiarjona <camarjona01@gmail.com>
2025-11-22 13:01:34 -0300

    formateos

--- frontend/find-fix-app/src/app/pages/admin-pages/oficios-list.page/oficios-list.page.ts
+++ frontend/find-fix-app/src/app/pages/admin-pages/oficios-list.page/oficios-list.page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { OficiosFormAdminComponent } from "../../../components/admin-components/oficios-form-admin-component/oficios-form-admin-component";
 import { OficiosListAdminComponent } from "../../../components/admin-components/oficios-list-admin-component/oficios-list-admin-component";
 
 @Component({
   selector: 'app-oficios-list.page',
-  imports: [OficiosFormAdminComponent, OficiosListAdminComponent],
+  imports: [OficiosListAdminComponent],
   templateUrl: './oficios-list.page.html',
   styleUrl: './oficios-list.page.css',
 })
