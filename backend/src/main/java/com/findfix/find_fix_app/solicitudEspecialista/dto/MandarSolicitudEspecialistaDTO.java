c394317df4768b72eb9504c48846eb151e5337fa
BelenIFigueroa <belenirmafigueroa@gmail.com>
2025-11-24 19:43:20 -0300

    Arreglos en nueva solicitud especialista

--- backend/src/main/java/com/findfix/find_fix_app/solicitudEspecialista/dto/MandarSolicitudEspecialistaDTO.java
+++ backend/src/main/java/com/findfix/find_fix_app/solicitudEspecialista/dto/MandarSolicitudEspecialistaDTO.java
@@ -1,13 +1,13 @@
 package com.findfix.find_fix_app.solicitudEspecialista.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record   MandarSolicitudEspecialistaDTO(
 
-        @Pattern(
-                regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ]+( [A-Za-zÁÉÍÓÚáéíóúÑñ]+)*$",
-                message = "No puede haber espacios al principio ni al final, ni multiples espacios seguidos."
-        )
+        @NotBlank(message = "El motivo es obligatorio.")
+        @Size(min = 10, max = 250, message = "El motivo debe tener entre 10 y 250 caracteres.")
         String motivo
 ){
 }
