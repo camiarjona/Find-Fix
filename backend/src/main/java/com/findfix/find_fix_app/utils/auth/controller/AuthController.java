d9b6d9d2c0d084667b9d33a35e94f74ee6aef6b1
camiarjona <camarjona01@gmail.com>
2026-01-31 19:15:13 -0300

    update - auth

--- backend/src/main/java/com/findfix/find_fix_app/utils/auth/controller/AuthController.java
+++ backend/src/main/java/com/findfix/find_fix_app/utils/auth/controller/AuthController.java
@@ -3,24 +3,23 @@ package com.findfix.find_fix_app.utils.auth.controller;
 import com.findfix.find_fix_app.usuario.dto.VerPerfilUsuarioDTO;
 import com.findfix.find_fix_app.usuario.model.Usuario;
 import com.findfix.find_fix_app.utils.apiResponse.ApiResponse;
+import com.findfix.find_fix_app.utils.auth.CookieUtil;
+import com.findfix.find_fix_app.utils.auth.dto.AuthResponseDTO;
 import com.findfix.find_fix_app.utils.auth.dto.RegistroDTO;
+import com.findfix.find_fix_app.utils.auth.dto.TokenRefreshResponseDTO;
 import com.findfix.find_fix_app.utils.auth.dto.UsuarioLoginDTO;
 import com.findfix.find_fix_app.utils.auth.service.AuthService;
 import com.findfix.find_fix_app.utils.exception.exceptions.RolException;
 import com.findfix.find_fix_app.utils.exception.exceptions.UsuarioException;
 import com.findfix.find_fix_app.utils.exception.exceptions.UsuarioNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,15 +28,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final CookieUtil  cookieUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<VerPerfilUsuarioDTO>> login(@Valid @RequestBody UsuarioLoginDTO loginDTO, HttpServletRequest request) throws UsuarioNotFoundException {
-        Usuario usuario = authService.login(loginDTO);
+    public ResponseEntity<ApiResponse<AuthResponseDTO>> login(@Valid @RequestBody UsuarioLoginDTO loginDTO) throws UsuarioNotFoundException {
+        AuthResponseDTO authResponse = authService.login(loginDTO);
 
-        HttpSession session = request.getSession(true);
-        session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+        ResponseCookie cookie = cookieUtil.crearCookieRefresh(authResponse.getRefreshToken());
+        authResponse.setRefreshToken(null);
 
-        return ResponseEntity.ok(new ApiResponse<>("Login exitoso, bienvenido!", new VerPerfilUsuarioDTO(usuario)));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new ApiResponse<>("Login exitoso, bienvenido!", authResponse));
     }
 
     //metodo para que un usuario se registre en el sistema
@@ -48,5 +50,26 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(
                 "Usuario registrado con éxito✅",
                 new VerPerfilUsuarioDTO(user)));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<ApiResponse<TokenRefreshResponseDTO>> refresh(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken) {
+
+        if (refreshToken == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse<>("Error: No se encontró la cookie de refresco", null));
+        }
+
+        TokenRefreshResponseDTO tokenDto = authService.refrescarToken(refreshToken);
+
+        return ResponseEntity.ok(new ApiResponse<>("Token refrescado correctamente", tokenDto));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<Void>> logout() {
+        ResponseCookie cookie = cookieUtil.limpiarCookie();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new ApiResponse<>("Sesión cerrada exitosamente", null));
     }
 }
--- backend/src/main/java/com/findfix/find_fix_app/utils/auth/service/AuthService.java
+++ backend/src/main/java/com/findfix/find_fix_app/utils/auth/service/AuthService.java
@@ -1,22 +1,18 @@
 package com.findfix.find_fix_app.utils.auth.service;
 
+import com.findfix.find_fix_app.utils.auth.dto.AuthResponseDTO;
 import com.findfix.find_fix_app.utils.auth.dto.RegistroDTO;
 import com.findfix.find_fix_app.usuario.model.Usuario;
+import com.findfix.find_fix_app.utils.auth.dto.TokenRefreshResponseDTO;
 import com.findfix.find_fix_app.utils.auth.dto.UsuarioLoginDTO;
 import com.findfix.find_fix_app.utils.exception.exceptions.RolException;
 import com.findfix.find_fix_app.utils.exception.exceptions.UsuarioException;
 import com.findfix.find_fix_app.utils.exception.exceptions.UsuarioNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface AuthService {
-
     String obtenerEmailUsuarioAutenticado();
     Usuario obtenerUsuarioAutenticado() throws UsuarioNotFoundException;
-
-    // LOGIN: Autentica y devuelve el Usuario
-    Usuario login(UsuarioLoginDTO loginDTO) throws UsuarioNotFoundException;
-
-    // metodo para registrar un nuevo user
-    @Transactional(rollbackFor = Exception.class)
+    AuthResponseDTO login(UsuarioLoginDTO loginDTO) throws UsuarioNotFoundException;
     Usuario registrarNuevoUsuario(RegistroDTO registroDTO) throws RolException, UsuarioException;
+    TokenRefreshResponseDTO refrescarToken(String refreshTokenStr);
 }
